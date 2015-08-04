299bee106f51f900e16921c4f362adf6f01c9dd0
Zetra <zetra@mail.ugm.ac.id>
2015-08-04 14:57:35 +0700

    add copyright

--- benihtes/src/main/java/id/zelory/benihtes/MainActivity.java
+++ benihtes/src/main/java/id/zelory/benihtes/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 Zetra.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package id.zelory.benihtes;
 
 import android.content.Intent;
@@ -13,7 +29,6 @@ import java.util.List;
 
 import butterknife.Bind;
 import id.zelory.benih.BenihActivity;
-import id.zelory.benih.controller.BenihController;
 import id.zelory.benih.util.BenihBus;
 import id.zelory.benih.util.BenihWorker;
 import id.zelory.benih.view.BenihRecyclerView;
